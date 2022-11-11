e48af019d407230229c8246da053e11dc1b70ad5
KavinVelan2007 <108147209+KavinVelan2007@users.noreply.github.com>
2022-11-11 14:23:27 +0530

    main.py
    Path Finding Visualizer

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import pygame
-from algorithm import return_neighbours,bfs,reconstruct_path
-from tkinter import messagebox
+import threading
+from utils import *
 
-pygame.font.init()
-
-class Visualizer:
+class Visualiser:
 
     def __init__(self,width,height,rows,cols):
         self.width = width
@@ -12,96 +10,115 @@ class Visualizer:
         self.rows = rows
         self.cols = cols
         self.display = pygame.display.set_mode((self.width,self.height))
-        pygame.display.set_caption('Shortest Path Finding Algorithm Visualiser')
-        self.grid = [[' ' for i in range(self.cols)] for j in range(self.rows)]
-        self.run = True
+        self.grid = [[' ' for _ in range(cols)] for _ in range(rows)]
+        self.path = []
+        self.queue = []
         self.start = None
         self.end = None
-        self.diagonal_traversal = False
-        self.path = []
+        self.run = True
 
     def main(self):
         while self.run:
-            if self.diagonal_traversal:
-                pygame.display.set_caption('Diagonal = True')
-            else:
-                pygame.display.set_caption('Diagonal = False')
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.run = False
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        if self.start and self.end:
+                            neighbours = return_neighbours(self.grid)
+                            thread = threading.Thread(target=self.bfs,args=[neighbours,self.start,self.end])
+                            thread.start()
+                    elif event.key == pygame.K_r:
+                        self.reset()
                 key = pygame.mouse.get_pressed()
                 if key[0]:
-                    if not self.start:
+                    if self.start is None:
                         x,y = pygame.mouse.get_pos()
-                        self.start = (y // (self.height // self.rows),x // (self.width // self.cols))
-                        self.grid[self.start[0]][self.start[1]] = 'S'
-                    elif not self.end:
+                        col,row = (x // (self.width // self.cols),y // (self.height // self.rows))
+                        self.start = (row,col)
+                        self.grid[row][col] = 'S'
+                    elif self.end is None:
                         x,y = pygame.mouse.get_pos()
-                        self.end = (y // (self.height // self.rows),x // (self.width // self.cols))
-                        self.grid[self.end[0]][self.end[1]] = 'E'
-                    elif self.start and self.end:
+                        col,row = (x // (self.width // self.cols),y // (self.height // self.rows))
+                        if self.start != (row,col):
+                            self.end = (row,col)
+                            self.grid[row][col] = 'E'
+                    elif self.end and self.start:
                         x,y = pygame.mouse.get_pos()
-                        self.pos = (y // (self.height // self.rows),x // (self.width // self.cols))
-                        if self.pos != self.start and self.pos != self.end:
-                            self.grid[self.pos[0]][self.pos[1]] = '#'
+                        col,row = (x // (self.width // self.cols),y // (self.height // self.rows))
+                        if (row,col) != self.start and (row,col) != self.end:
+                            self.grid[row][col] = '#'
                 elif key[2]:
-                    x,y = pygame.mouse.get_pos()
-                    row,col = (y // (self.height // self.rows),x // (self.width // self.cols))
-                    if (row,col) == self.start:
-                        self.start = None
-                        self.grid[row][col] = ' '
-                    elif (row,col) == self.end:
-                        self.end = None
-                        self.grid[row][col] = ' '
-                    else:
-                        self.grid[row][col] = ' '
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_SPACE and self.start and self.end:
-                        neighbours = return_neighbours(self.grid,self.diagonal_traversal)
-                        path = bfs(neighbours,self.start,self.end)
-                        if path:
-                            reconstruct_path(self.grid,path)
-                    elif event.key == pygame.K_c:
-                        self.reset()
-                    elif event.key == pygame.K_r:
-                        self.remove_path()
-                    elif event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                        if self.diagonal_traversal:
-                            self.diagonal_traversal = False
+                    if self.start:
+                        x,y = pygame.mouse.get_pos()
+                        col,row = (x // (self.width // self.cols),y // (self.height // self.rows))
+                        if self.grid[row][col] == 'S':
+                            self.start = None
+                            self.grid[row][col] = ' '
+                        elif self.grid[row][col] == 'E':
+                            self.end = None
+                            self.grid[row][col] = ' '
                         else:
-                            self.diagonal_traversal = True
-
+                            self.grid[row][col] = ' '
             self.draw(self.display)
-            pygame.display.update()
 
         pygame.quit()
-
-    def reset(self):
-        self.start = None
-        self.end = None
-        self.grid = [[' ' for i in range(self.cols)] for j in range(self.rows)]
-
-    def remove_path(self):
-        for i in range(len(self.grid)):
-            for j in range(len(self.grid[i])):
-                if self.grid[i][j] == '*':
-                    self.grid[i][j] = ' '
-
+        
     def draw(self,win):
         win.fill((255,255,255))
-        for i in range(self.rows):
-            for j in range(self.cols):
-                if self.grid[i][j] == 'S':
-                    pygame.draw.rect(win,(255,0,0),(j * (self.width // self.cols),i * (self.height // self.rows),self.width // self.cols,self.height // self.rows))
-                elif self.grid[i][j] == 'E':
-                    pygame.draw.rect(win,(255,0,0),(j * (self.width // self.cols),i * (self.height // self.rows),self.width // self.cols,self.height // self.rows))
-                elif self.grid[i][j] == '#':
-                    pygame.draw.rect(win,(0,0,0),(j * (self.width // self.cols),i * (self.height // self.rows),self.width // self.cols,self.height // self.rows))
-                elif self.grid[i][j] == '*':
-                    pygame.draw.rect(win,(255,255,0),(j * (self.width // self.cols),i * (self.height // self.rows),self.width // self.cols,self.height // self.rows))
-        for i in range(self.cols):
-            for j in range(self.rows):
-                pygame.draw.rect(win,(100,100,100),(j * (self.width // self.cols),i * (self.height // self.rows),self.width // self.cols,self.height // self.rows),1)
+        for index,row in enumerate(self.grid):
+            for index2,col in enumerate(self.grid[index]):
+                if col == '#':
+                    pygame.draw.rect(win,(0,0,0),(index2 * (self.width // self.cols),index * (self.height // self.rows),self.width // self.cols,self.height // self.rows))
+                elif col == 'S':
+                    pygame.draw.rect(win,(255,0,0),(index2 * (self.width // self.cols),index * (self.height // self.rows),self.width // self.cols,self.height // self.rows))
+                elif col == 'E':
+                    pygame.draw.rect(win,(0,0,255),(index2 * (self.width // self.cols),index * (self.height // self.rows),self.width // self.cols,self.height // self.rows))                
+                elif col == '*':
+                    pygame.draw.rect(win,(255,255,0),(index2 * (self.width // self.cols),index * (self.height // self.rows),self.width // self.cols,self.height // self.rows))
+                elif col == '+':
+                    pygame.draw.rect(win,(128,0,128),(index2 * (self.width // self.cols),index * (self.height // self.rows),self.width // self.cols,self.height // self.rows))
+        for row,col in self.path:
+            self.grid[row][col] = '+'
+        for row,col in self.queue:
+            pygame.draw.rect(win,(0,255,0),(col * (self.width // self.cols),row * (self.height // self.rows),self.width // self.cols,self.height // self.rows))
+        for i in range(0,self.height,self.height // self.rows):
+            for j in range(0,self.width,self.width // self.cols):
+                pygame.draw.rect(win,(128,128,128),(j,i,self.width // self.cols,self.height // self.rows),1)
+        pygame.display.update()
 
-vis = Visualizer(750,750,25,25)
+    def reset(self):
+        self.grid = [[' ' for _ in range(self.cols)] for _ in range(self.rows)]
+        self.path = []
+        self.queue = []
+        self.start,self.end = None,None
+
+    def bfs(self,neighbours,start,end):
+        visited = []
+        self.queue = [start]
+        predecessorNodes = {}
+
+        while self.queue:
+            if self.run:
+                node = self.queue.pop(0)
+                for neighbour in neighbours[node]:
+                    if neighbour not in visited:
+                        visited.append(neighbour)
+                        self.queue.append(neighbour)
+                        predecessorNodes[neighbour] = node
+                        if self.end in predecessorNodes:
+                            self.queue.clear()
+                        row,col = neighbour
+                        if (row,col) != start:
+                            self.grid[row][col] = '*'
+                        pygame.time.delay(15)
+        if self.run:
+            currentNode = end
+            while currentNode != start:
+                currentNode = predecessorNodes[currentNode]
+                if currentNode != start and currentNode != end:
+                    self.path.append(currentNode)
+                pygame.time.delay(15)
+                
+vis = Visualiser(750,750,30,30)
 vis.main()
