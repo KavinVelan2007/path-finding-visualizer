82f14730a67d4be8231464009d1e5285b0bc5eed
KavinVelan2007 <108147209+KavinVelan2007@users.noreply.github.com>
2022-11-11 14:28:13 +0530

    utils.py

--- algorithm.py
+++ algorithm.py
@@ -4,7 +4,7 @@ def return_neighbours(grid):
         for j in range(len(grid[i])):
             neighbours[(i,j)] = []
             try:
-                if grid[i-1][j] and grid[i-1][j] != '#' and i-1 >= 0: #UP
+                if grid[i-1][j] and grid[i-1][j] != '#' and i-1 >= 0:
                     neighbours[(i,j)].append((i-1,j))
             except:
                 None
